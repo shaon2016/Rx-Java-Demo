a147d55cc928ecd3d76e22f14461ded54882a0e2
shaon2016 <shaondeveloper@gmail.com>
2017-12-31 18:16:54 +0600

    project structure changed

--- app/src/main/java/com/durbinlabs/rxjavademo/activities/MainActivity.java
+++ app/src/main/java/com/durbinlabs/rxjavademo/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.durbinlabs.rxjavademo;
+package com.durbinlabs.rxjavademo.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.durbinlabs.rxjavademo.R;
 
 public class MainActivity extends AppCompatActivity {
 
